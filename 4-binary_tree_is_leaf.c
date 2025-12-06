c030d050d13cb96473c52f4127ce902830b6583c
titomundo <tito.alberto.mundo@gmail.com>
2025-12-05 23:28:28 -0600

    task 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_is_leaf - checks if a node is a leaf
+* @node: node to check
+*
+* Return: 1 if is leaf, 0 otherwise
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node || node->left || node->right)
+		return (0);
+	else
+		return (1);
+}
