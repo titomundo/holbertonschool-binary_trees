d90e701def2534c362af594edeea00bc746c60a3
titomundo <tito.alberto.mundo@gmail.com>
2025-12-05 22:21:22 -0600

    set left node to null

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -21,7 +21,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	new->n = value;
 	new->parent = parent;
-	new->right = NULL;
+	new->left = NULL;
 
 	if (parent->right)
 	{
