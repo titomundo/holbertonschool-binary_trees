978185e25ca9201a5d19ef7d6009f7e5ac215541
titomundo <tito.alberto.mundo@gmail.com>
2025-12-05 23:22:47 -0600

    task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_delete - deletes an entire binary tree
+* @tree: tree to DESTROY
+*
+* Return: void
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
+}
