f47d7eea0bc01c82b37aecb07abf68c2a31be562
titomundo <tito.alberto.mundo@gmail.com>
2025-12-07 14:11:44 -0600

    task 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_postorder - travels trough a binary tree in postorder
+* @tree: tree to DESTROY
+* @func: pointer to a function
+*
+* Return: void
+*/
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
