51c09245859654e086c1a7a14b75975ea0686634
titomundo <tito.alberto.mundo@gmail.com>
2025-12-07 20:59:51 -0600

    task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_height - gets the maximum height of a binary tree
+* @tree: tree to check
+*
+* Return: maximum height of tree
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int right_h, left_h, max;
+
+	if (!tree)
+		return (-1);
+
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
+
+	if (left_h > right_h)
+		max = left_h;
+	else
+		max = right_h;
+
+	return (max + 1);
+}
